f213a3a838fb83d4e3e26b32adf21862938492b2
Raimondas Rimkevičius <github@mekdrop.name>
2021-02-09 01:44:57 +0200

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # log-data-output-decorator
-Small decorator that extends @Symfony OutputInterface delivered class with few options for easier to log data
+
+[![License](https://img.shields.io/github/license/imponeer/log-data-output-decorator.svg)](LICENSE)
+[![GitHub release](https://img.shields.io/github/release/imponeer/log-data-output-decorator.svg)](https://github.com/imponeer/log-data-output-decorator/releases) [![Maintainability](https://api.codeclimate.com/v1/badges/3e11a2969204f2abc7cf/maintainability)](https://codeclimate.com/github/imponeer/log-data-output-decorator/maintainability) [![PHP](https://img.shields.io/packagist/php-v/imponeer/log-data-output-decorator.svg)](http://php.net) 
+[![Packagist](https://img.shields.io/packagist/dm/imponeer/log-data-output-decorator.svg)](https://packagist.org/packages/imponeer/log-data-output-decorator)
+
+# Smarty DB resource
+
+Small decorator that extends [Symfony OutputInterface](https://github.com/symfony/console/blob/5.x/Output/OutputInterface.php) delivered class with few options for easier to log data.
+
+## Installation
+
+To install and use this package, we recommend to use [Composer](https://getcomposer.org):
+
+```bash
+composer require imponeer/log-data-output-decorator
+```
+
+Otherwise, you need to include manually files from `src/` directory. 
+
+## Usage example
+
+```php
+$output = new \Imponeer\Decorators\LogDataOutput\OutputDecorator(
+   new \Symfony\Component\Console\Output\BufferedOutput();
+);
+$output->info('test');
+$output->incrIndent();
+$output->info('#1');
+$output->info('#2');
+``
+
+## Using from templates
+
+To use this resource from templates, you need to use `db:` prefix when accessing files. For example :
+```smarty
+  {include file="db:/images/image.tpl"}
+```
+
+## How to contribute?
+
+If you want to add some functionality or fix bugs, you can fork, change and create pull request. If you not sure how this works, try [interactive GitHub tutorial](https://try.github.io).
+
+If you found any bug or have some questions, use [issues tab](https://github.com/imponeer/log-data-output-decorator/issues) and write there your questions.
---
+++ src/OutputDecorator.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace Imponeer\Decorators\LogDataOutput;
+
+use Symfony\Component\Console\Formatter\OutputFormatterInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Small decorator that extends @Symfony OutputInterface delivered class with few options for easier to log data
+ *
+ * @package Imponeer\Decorators\LogDataOutput
+ */
+class OutputDecorator implements OutputInterface
+{
+    /**
+     * Ident for each line in left
+     *
+     * @var int
+     */
+    protected $indent = 0;
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * OutputDecorator constructor.
+     *
+     * @param OutputInterface $originalOutput Output interface where this decorator will write
+     */
+    public function __construct(OutputInterface $originalOutput)
+    {
+        $this->output = $originalOutput;
+    }
+
+    /**
+     * Increases indent
+     */
+    public function incrIndent(): void
+    {
+        $this->indent++;
+    }
+
+    /**
+     * Decreases indent
+     */
+    public function decrIndent(): void
+    {
+        $this->indent--;
+    }
+
+    /**
+     * Resets indent
+     */
+    public function resetIndent(): void
+    {
+        $this->indent = 0;
+    }
+
+    /**
+     * Prints fatal message
+     *
+     * @param string $message Message to print
+     * @param mixed ...$params Params for parsing message
+     */
+    public function fatal(string $message, ...$params): void
+    {
+        $this->write('<error>' . vsprintf($message, $params) . '</error>', true);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function write($messages, $newline = false, $options = self::OUTPUT_NORMAL)
+    {
+        if ($this->indent > 0) {
+            $tmpMsg = '';
+            foreach (explode(PHP_EOL, $messages) as $line) {
+                $tmpMsg .= str_repeat(' ', $this->indent * 2) . $line . PHP_EOL;
+            }
+            $messages = rtrim($tmpMsg);
+        }
+        /*if (trim($messages) == '') {
+            return $this->output->write(var_export([$messages, debug_backtrace(false)[1]], true), $newline, $options);
+        }*/
+        return $this->output->write($messages, $newline, $options);
+    }
+
+    /**
+     * Prints success message
+     *
+     * @param string $message Message to print
+     * @param mixed ...$params Params for parsing message
+     */
+    public function success(string $message, ...$params): void
+    {
+        $this->write('<info>' . vsprintf($message, $params) . '</info>', true);
+    }
+
+    /**
+     * Prints error message
+     *
+     * @param string $message Message to print
+     * @param mixed ...$params Params for parsing message
+     */
+    public function error(string $message, ...$params): void
+    {
+        $this->write('<error>' . vsprintf($message, $params) . '</error>', true);
+    }
+
+    /**
+     * Prints info message
+     *
+     * @param string $message Message to print
+     * @param mixed ...$params Params for parsing message
+     */
+    public function info(string $message, ...$params): void
+    {
+        $this->write('<comment>' . vsprintf($message, $params) . '</comment>', true);
+    }
+
+    /**
+     * Prints simple message
+     *
+     * @param string $message Message to print
+     * @param mixed ...$params Params for parsing message
+     */
+    public function msg(string $message, ...$params): void
+    {
+        $this->write(vsprintf($message, $params), true);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function writeln($messages, $options = 0)
+    {
+        $this->write($messages, true, $options);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setVerbosity($level)
+    {
+        $this->output->setVerbosity($level);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getVerbosity()
+    {
+        return $this->output->getVerbosity();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isQuiet()
+    {
+        return $this->output->isQuiet();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isVerbose()
+    {
+        return $this->output->isVerbose();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isVeryVerbose()
+    {
+        return $this->output->isVeryVerbose();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isDebug()
+    {
+        return $this->output->isDebug();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setDecorated($decorated)
+    {
+        $this->output->setDecorated($decorated);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isDecorated()
+    {
+        return $this->output->isDecorated();
+    }
+
+    /**
+     * Sets current output formatter instance.
+     *
+     * @param OutputFormatterInterface $formatter Output formatter
+     */
+    public function setFormatter(OutputFormatterInterface $formatter)
+    {
+        $this->output->setFormatter($formatter);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFormatter()
+    {
+        return $this->output->getFormatter();
+    }
+}
